56100c734652532197572e5e16cd1ee7d5d55a76
syailian <kaikaistar4@163.com>
2020-09-04 15:57:13 +0800

    初始化创建

---
+++ ivocmn-admin/src/main/java/com/ivocmn/shg/IvocmnAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ivocmn.shg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvocmnAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvocmnAdminApplication.class, args);
+    }
+
+}
---
+++ ivocmn-algorithm/src/main/java/com/ivocmn/shg/IvocmnAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.ivocmn.shg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvocmnAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvocmnAlgorithmApplication.class, args);
+    }
+
+}
